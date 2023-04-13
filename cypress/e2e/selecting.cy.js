20d2c51fb05400f1a18ed42298a97c36e691f2d9
juliasut <juliasutygina@gmail.com>
2023-04-12 18:22:34 -0600

    use more robust selection with data-cy attributes

---
+++ cypress/e2e/selecting.cy.js
@@ -0,0 +1,25 @@
+describe('Text box with max characters', () => {
+  it('displays the appropriate remaining characters count', () => {
+    cy.visit('http://localhost:3000/example-3');
+    cy.get('[data-cy="first-name-chars-left-count"]')
+      .invoke('text')
+      .should('equal', '15');
+
+    cy.get('[data-cy="input-first-name"]').type('hello');
+    cy.get('[data-cy="first-name-chars-left-count"]')
+      .invoke('text')
+      .should('equal', '10');
+
+    cy.get('[data-cy="input-first-name"').type(' my friend');
+    cy.get('[data-cy="first-name-chars-left-count"]')
+      .invoke('text')
+      .should('equal', '0');
+  });
+
+  it('prevents the user from typing more characters once max is exceded', () => {
+    cy.visit('http://localhost:3000/example-3');
+
+    cy.get('[data-cy="input-first-name"]').type('moreThanFifteenCharacters');
+    cy.get('[data-cy="input-first-name"]').should('have.attr', 'value', 'moreThanFifteen');
+  });
+});
